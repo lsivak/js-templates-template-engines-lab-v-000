3d3242507cc41522ce7777a62135011c39381b0d
Lara Sanders <larasanders@gmail.com>
2018-07-11 17:14:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ function postComment() {
   var commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
 
   var commentText = document.getElementById("commentText").value;
-  var commenterName = document.getElementById("commenter").value;
+  var commenterName = document.getElementById("commenterName").value;
 
   var commentsSection = document.getElementById("comments");
-  commentsSection.innerHTML += commentTemplate({ 'commenter': commenterName, 'comment': commentText });
+  commentsSection.innerHTML += commentTemplate({ 'commenterName': commenterName, 'comment': commentText });
 }
